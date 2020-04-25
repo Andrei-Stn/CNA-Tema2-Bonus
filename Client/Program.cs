eb35b26d5d767881ff5d3bc0518b10887454c5e5
AndreiStan <andystan291@yahoo.com>
2020-04-25 21:51:32 +0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Grpc.Net.Client;
+using Tema2CNA.Protos;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var input = ValidateInput();
+
+            var season = GetSeason(input);
+
+            switch (season)
+            {
+                case "Winter":
+                {
+                    var client = new Winter.WinterClient(channel);
+                    var request = new WinterRequest();
+                    request.Date = input;
+                    var response = client.GetWinterSign(request);
+                    Console.WriteLine("You are born in Winter.");
+                    Console.WriteLine($"Your sign is {response.Sign}");
+                    break;
+                }
+
+                case "Spring":
+                {
+                    var client = new Spring.SpringClient(channel);
+                    var request = new SpringRequest();
+                    request.Date = input;
+                    var response = client.GetSpringSign(request);
+                    Console.WriteLine("You are born in Spring.");
+                    Console.WriteLine($"Your sign is {response.Sign}");
+                    break;
+                }
+
+                case "Summer":
+                {
+                    var client = new Summer.SummerClient(channel);
+                    var request = new SummerRequest();
+                    request.Date = input;
+                    var response = client.GetSummerSign(request);
+                    Console.WriteLine("You are born in Summer.");
+                    Console.WriteLine($"Your sign is {response.Sign}");
+                    break;
+                }
+
+                case "Autumn":
+                {
+                    var client = new Autumn.AutumnClient(channel);
+                    var request = new AutumnRequest();
+                    request.Date = input;
+                    var response = client.GetAutumnSign(request);
+                    Console.WriteLine("You are born in Autumn");
+                    Console.WriteLine($"Your sign is {response.Sign}");
+                    break;
+                }
+            }
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static string ValidateInput()
+        {
+            Console.WriteLine("Enter your birthdate:");
+            var input = Console.ReadLine();
+
+            while (true)
+            {
+                DateTime dateTime;
+                if (!DateTime.TryParseExact(input, "MM/dd/yyyy", null, DateTimeStyles.None, out dateTime))
+                {
+                    Console.WriteLine("Invalid date input.");
+                    input = Console.ReadLine();
+                }
+                return input;
+            }
+        }
+        public static string GetSeason(string date)
+        {
+            DateTime dateTime = DateTime.Parse(date);
+            switch (dateTime.Month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Winter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Spring";
+                case 6:
+                case 7:
+                case 8:
+                    return "Summer";
+                default:
+                    return "Autumn";
+            }
+        }
+
+    }
+}
---
+++ Tema2CNA/Services/SpringService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Tema2CNA.Protos;
+
+namespace Tema2CNA.Services
+{
+    public class SpringService: Spring.SpringBase
+    {
+        public override Task<SpringResponse> GetSpringSign(SpringRequest request, ServerCallContext context)
+        {
+            var birthday = request.Date;
+            var year = GetYears(birthday);
+            var dateTime = DateTime.Parse(birthday);
+            var path = "Spring.txt";
+            var file = File.ReadAllLines(path);
+
+            string sign = GetSign(file, year, dateTime);
+
+            Console.WriteLine("Your sign is : " + sign);
+            return Task.FromResult(new SpringResponse() { Sign = sign });
+        }
+        //converts requested date from string to DateTime
+        public static DateTime GetDate(string date, int year)
+        {
+            DateTime dateTime = DateTime.Parse(date);
+            var birthYears = dateTime.AddYears(-dateTime.Year + year);
+
+            return birthYears;
+        }
+
+
+        //get the years from birthday
+        public static int GetYears(string date)
+        {
+            string[] pieces = date.Split('/');
+
+            return int.Parse(pieces[2]);
+        }
+
+        //return the sign
+        public static string GetSign(string[] files, int year, DateTime birthdate)
+        {
+            DateTime startDay;
+            DateTime endDay;
+            string sign;
+
+            for (int i = 0; i < files.Length; i += 3)
+            {
+                sign = files[i];
+                startDay = GetDate(files[i + 1], year);
+                endDay = GetDate(files[i + 2], year);
+                if (startDay <= birthdate && birthdate <= endDay)
+                {
+                    return sign;
+                }
+            }
+
+            return "Eroare";
+        }
+    }
+}
